1c2e1a688d0163485162325d54bb9468c0d3f853
Eduardo Tonon <lynselite@gmail.com>
2025-12-24 02:26:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1798437152-d9134",
+  "appId": "1:876349815274:web:d57f0931a11fb427495a93",
+  "apiKey": "AIzaSyBixlS_GwWTSzZ7kU-tfz7GMYfVf9PjZCQ",
+  "authDomain": "studio-1798437152-d9134.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "876349815274"
+};
